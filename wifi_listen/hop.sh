84dcaa01bf6ba094c65af2ab185a8840744a7bb8
trainman419 <legotown@aol.com>
2012-10-23 18:38:16 -0700

    Updated helper scripts.

--- wifi_listen/hop.sh
+++ wifi_listen/hop.sh
@@ -4,7 +4,7 @@ while true
 do
   for c in 01 03 05 07 09 11 13 149 153 159 165 38 44 48 02 04 06 08 10 12 14 151 157 161 36 40 46 
   do
-    iwconfig wlan1 channel $c
+    iwconfig $1 channel $c
     echo $c
     sleep 3
   done
